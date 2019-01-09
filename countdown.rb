2d71c4b6352e25f8ad8010df9c2d37169c82c162
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-09 16:49:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,15 @@ def countdown(num)
     until num == 0
       puts "#{num} SECOND(S)!"
       num -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+
+def countdown(num)
+    until num == 0
+      puts "#{num} SECOND(S)!"
+      num -= 1
       sleep(1)
     end
     "HAPPY NEW YEAR!"
